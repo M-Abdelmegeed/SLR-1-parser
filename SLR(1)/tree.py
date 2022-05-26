17d7fa593ec6b47e7f95d9ac688e80bddebfbc08
Mohammed Abdelmegeed <90607257+M-Abdelmegeed@users.noreply.github.com>
2022-05-26 11:46:40 +0200

    Add files via upload

---
+++ SLR(1)/tree.py
@@ -0,0 +1,118 @@
+import nltk
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+from nltk import pos_tag, word_tokenize, RegexpParser
+# Obtaining the token list
+file = open("test2.txt")
+import re
+
+operators = {'=':'EQUAL',':=' : 'ASSIGN','+' : 'PLUS','-' : 'MINUS','/' : 'DIV','*' : 'MULT','<' : 'LESSTHAN','>' : 'GREATERTHAN' }
+operators_key = operators.keys()
+
+data_type = {'int' : 'INTEGER TYPE', 'float': 'FLOATING POINT' , 'char' : 'CHARACTER TYPE', 'long' : 'LONG INT' }
+data_type_key = data_type.keys()
+
+punctuation_symbol = { ':' : 'COLON', ';' : 'SEMI-COLON', '.' : 'DOT' , ',' : 'COMMA' }
+punctuation_symbol_key = punctuation_symbol.keys()
+
+identifier = {"ID":['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', "n", "o", "p", "q", "r", "s", 't', 'u', 'v', "x", 'y','z','xyz','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', "N", "O", "P", "Q", "R", "S", 'T', 'U', 'V', "X", 'Y','Z','XYZ']}
+identifier_key = identifier.keys()
+identifier_values=identifier.values()
+arrval=[]
+for value in identifier_values:
+    arrval=value
+
+
+key_words={'if':'IF', 'then':'THEN', 'end':'END', 'else':'ELSE','read':'READ', 'write':'WRITE', 'until':'UNTIL', 'repeat':'REPEAT'}
+key_word_keys=key_words.keys()
+
+
+a_list = list(range(1, 10000))
+a_list=str(a_list)
+numbers={"NUMBER":a_list}
+number_values=numbers.values()
+arrvalnum=[]
+for value in number_values:
+    arrvalnum=value
+for i in range(0, len(arrvalnum)):
+    if arrvalnum[i]=="":
+        arrvalnum.remove(arrvalnum[i])
+dataFlag = False
+
+a=file.read()
+tokenType=[];
+oringinalTokens=[];
+originalTokens=a.split(" ")
+count=0
+program = a.split("\n")
+for line in program:
+    count = count + 1
+    # print("line#" , count, "\n" , line)
+    tokens=line.split(' ')
+    while("" in tokens) :
+        tokens.remove("")
+    # # print("Tokens are " , tokens)
+    # print("Line#", count, "properties \n")
+    for token in tokens:
+        if token in operators_key:
+            # print('<' + token + " , " + operators[token] + '>')
+            tokenType.append(operators[token]);
+        elif token in data_type_key:
+            # print("datatype is", data_type[token])
+            tokenType.append(data_type[token]);
+        elif token in punctuation_symbol_key:
+            # print ('<' + token + " , " + punctuation_symbol[token] + '>')
+            tokenType.append(punctuation_symbol[token]);
+        elif token in arrvalnum:
+            # print ('<' + token + " , " + "NUMBER" + '>')
+            tokenType.append("NUMBER");
+        elif token in key_words:
+            # print ('<' + token + " , " + key_words[token] + '>' )
+            tokenType.append(key_words[token]);
+                # if token in arrval:
+        elif bool(re.match('[A-Za-z][A-Za-z0-9]*', token)):
+            # print('<' + token + " , " + "ID" + '>')
+            tokenType.append("ID");
+                  
+    dataFlag=False
+    # print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+    # print("")
+    # print(tokenType)
+    # print(originalTokens)
+
+
+# Example text
+
+# Find all parts of speech in above sentence
+# tagged = pos_tag(tokenType)
+# print(tagged)
+
+
+tagged2 = list(zip(originalTokens,tokenType))
+print(tagged2)
+
+#Extract all parts of speech from any text
+# case_grammar = RegexpParser("""
+#                             stmt-seq: {<stmt-seq statement | statement>} #
+#                             statement: {<if-stmt | assign-stmt >}#
+#                             if-stmt: {<IF NUMBER THEN stmt-seq END >}#
+#                             assign-stmt: {<ID ASSIGN factor SEMI-COLON>} #
+#                             factor: {<ID | NUMBER>} #
+#                             """)
+case_grammar2 =         nltk.CFG.fromstring("""
+                            stmt-seq -> stmt-seq statement | statement
+                            statement -> if-stmt | assign-stmt
+                            if-stmt -> 'IF' 'NUMBER' 'THEN' stmt-seq 'END'
+                            assign-stmt -> 'ID' 'ASSIGN' factor 'SEMI-COLON'
+                            factor -> 'ID' | 'NUMBER'
+                            """)
+
+# Print all parts of speech in above sentence
+# output = case_grammar.parse(tagged2)
+
+parser = nltk.ChartParser(case_grammar2)
+output2 =parser.parse(tokenType)
+
+for tree in parser.parse(tokenType):
+    print(tree)
+    tree.draw()
